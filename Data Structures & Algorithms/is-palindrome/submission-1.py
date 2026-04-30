1dba0bf2c2ca4ee9e87cd2d07c6b2efeff8e638b
Hexacom06 <90303360+Hexacom06@users.noreply.github.com>
2026-04-30 18:00:09 +0530

    Add: is-palindrome - submission-1

---
+++ Data Structures & Algorithms/is-palindrome/submission-1.py
@@ -0,0 +1,8 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        clean = "".join(char.lower() for char in s if char.isalnum())
+        n = len(clean)
+        for i in range((n//2)):
+            if clean[i] != clean[n-1-i]:
+                return False
+        return True
