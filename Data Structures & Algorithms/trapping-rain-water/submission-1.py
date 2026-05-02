a1768fe8cc25e3e73647e1799a0459f89aa97ae6
Hexacom06 <90303360+Hexacom06@users.noreply.github.com>
2026-05-03 01:15:59 +0530

    Add: trapping-rain-water - submission-1

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-1.py
@@ -0,0 +1,21 @@
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        left_max = []
+        right_max = [0]*len(height)
+        n,left,right = len(height),0,0
+        for h in height:
+            if left < h:
+                left = h
+            left_max.append(left)
+        for i in range(n-1, -1, -1):
+            if right < height[i]:
+                right = height[i]
+            right_max[i] = right
+        area = 0
+        for i in range(n):
+            temp = min(left_max[i],right_max[i])-height[i]
+            if temp > 0:
+                area += temp
+        return area
+
+        
