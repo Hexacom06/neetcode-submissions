ca9badff006f3bbd34730a2ce44d89118aa7219d
Hexacom06 <90303360+Hexacom06@users.noreply.github.com>
2026-05-11 23:01:19 +0530

    Add: search-for-word - submission-1

---
+++ Data Structures & Algorithms/search-for-word/submission-1.py
@@ -0,0 +1,28 @@
+class Solution:
+    def exist(self, board: List[List[str]], word: str) -> bool:
+        def traverse(i,j,ind):
+            if ind == len(word): return True
+            
+            if i < 0 or i >= len(board) or j < 0 or j >= len(board[0]): return False
+            if word[ind] != board[i][j]: return False
+            if (i,j) in visited: return False
+
+            visited.add((i,j))
+            res = traverse(i+1,j,ind+1) or traverse(i-1,j,ind+1) or traverse(i,j+1,ind+1) or traverse(i,j-1,ind+1)
+            visited.remove((i,j))
+
+            return res
+
+        visited = set()
+        for i,row in enumerate(board):
+            for j,c in enumerate(row):
+                if c == word[0]:
+                   if traverse(i,j,0):
+                    return True
+        return False
+
+        
+
+
+
+
