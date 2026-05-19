defd5cd278d09868726e7991ff957a795b2c3c90
Hexacom06 <90303360+Hexacom06@users.noreply.github.com>
2026-05-19 15:30:36 +0530

    Add: house-robber - submission-4

---
+++ Data Structures & Algorithms/house-robber/submission-4.py
@@ -0,0 +1,9 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        n = len(nums)+1
+        dp = [0] * n
+        dp[0] = 0
+        dp[1] = nums[0]
+        for i in range(2,n):
+          dp[i] = max(dp[i-2] + nums[i-1], dp[i-1])
+        return dp[n-1]
