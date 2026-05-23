f7e67bff495d394545612b33b982aac03015911f
Hexacom06 <90303360+Hexacom06@users.noreply.github.com>
2026-05-23 17:00:35 +0530

    Add: max-area-of-island - submission-5

---
+++ Data Structures & Algorithms/max-area-of-island/submission-5.py
@@ -0,0 +1,17 @@
+class Solution:
+    def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
+        maxArea = 0
+        visited = set()
+        ROW = len(grid)
+        COL = len(grid[0])
+        def traversal(r,c):
+            if r < 0 or r >= ROW or c < 0 or c >= COL or not grid[r][c] or (r,c) in visited: return 0
+            visited.add((r,c))
+            return traversal(r-1,c) + traversal(r,c-1) + traversal(r+1,c) + traversal(r,c+1) + 1
+            return 
+        for i in range(ROW):
+            for j in range(COL):
+                if grid[i][j] and (i,j) not in visited:
+                    area = traversal(i,j)
+                    maxArea = max(area, maxArea)
+        return maxArea
